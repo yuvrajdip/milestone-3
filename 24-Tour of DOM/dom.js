9e677d0ff4c37d244acdca79f4ab887be1d99f0b
yuvrajdip <dipbhowmik2018@gmail.com>
2023-08-14 01:17:17 +0600

    getElementsByTagName()

--- 24-Tour of DOM/dom.js
+++ 24-Tour of DOM/dom.js
@@ -1,4 +1,16 @@
 console.log("hello from js")
 console.log(document);
 
-console.log(document.getElementsByTagName('h1'))  
+// todo getElementsByTagName()
+const liCollection = document.getElementsByTagName('li')
+
+for(const li of liCollection){
+  console.log(li);
+  console.log(li.innerText);
+}
+
+const h1Collection = document.getElementsByTagName('h1');
+for(const h1 of h1Collection){
+  console.log(h1);
+  console.log(h1.innerText);
+}
