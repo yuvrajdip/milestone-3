64790edd08ec93e8d34de95740c540eb25372713
yuvrajdip <dipbhowmik2018@gmail.com>
2023-08-14 11:19:17 +0600

    getElementById getElementsByClassName

--- 24-Tour of DOM/dom.js
+++ 24-Tour of DOM/dom.js
@@ -1,7 +1,8 @@
 console.log("hello from js")
 console.log(document);
 
-// todo getElementsByTagName()
+// todo   getElementsByTagName()
+
 const liCollection = document.getElementsByTagName('li')
 
 for(const li of liCollection){
@@ -13,4 +14,17 @@ const h1Collection = document.getElementsByTagName('h1');
 for(const h1 of h1Collection){
   console.log(h1);
   console.log(h1.innerText);
+}
+
+//* getElementById returns only one element
+const fruits= document.getElementById("fruit-title")
+console.log(fruits);
+
+
+//* getElementsByClassName returns an Array(HTML Collections)
+const liItems = document.getElementsByClassName("li-item")
+
+console.log(liItems);
+for(const li of liItems){
+  console.log(li.innerText);
 }
